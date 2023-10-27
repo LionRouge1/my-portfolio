260281082945b7b9f0bb38e065d3f50b3ef4a498
LionRouge1 <matchoudiavlessi@gmail.com>
2023-10-27 10:32:31 +0000

    implement mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Profile from '../assets/profile.jpeg';
-import '../styles/header.css';
+import '../styles/header.scss';
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { IoClose } from 'react-icons/io5';
 
 const Header = () => {
+  const [close, setClose] = useState(false);
+
+  const handlehamburger = () => {
+    setClose((prev) => (prev ? false : true))
+  }
   return (
     <header>
       <Link to="/" className="profile-image">
         <img src={Profile} alt="Matchoudi Avlessi" />
       </Link>
-      <nav>
-        <GiHamburgerMenu />
+      <nav className={close ? 'active' : ''}>
+        {
+          close ? <IoClose onClick={handlehamburger} /> : <GiHamburgerMenu onClick={handlehamburger} />
+        }
         <ul>
           <li><a href="#">Projects</a></li>
           <li><a href="#">Articles</a></li>
