4b430aeb7789cec4ea301681bff073f62314efba
LionRouge1 <matchoudiavlessi@gmail.com>
2024-03-11 17:09:59 +0000

    Add thank you page link to the App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ViewContextProvider } from './components/context/getviewContext';
 import Home from './components/Home';
 import Articles from './components/Articles';
 import NotFound from './components/NotFound';
+import Thanks from './components/Thanks';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='/articles' element={<Articles />} />
+          <Route path='/thanks' element={<Thanks />} />
           <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
