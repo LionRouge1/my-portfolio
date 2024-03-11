2355c73fd2a7dcd65834a951c4b2bca53dfd1b60
LionRouge1 <matchoudiavlessi@gmail.com>
2024-03-11 17:13:39 +0000

    Add formspree form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,16 @@
 import '../styles/contact.scss';
+import { useForm } from '@formspree/react';
 import { Ell8, Group1, Group2, Group3 } from './Design';
+import { useNavigate } from 'react-router-dom';
 
-const Contact = () => (
+const Contact = () => {
+  const [state, handleSubmit] = useForm("mjvnookq");
+  const navigate = useNavigate();
+  if (state.succeeded) {
+    navigate('/thanks', {state: {sendMail: true}});
+  }
+
+  return (
   <section className="contact-me" id='contact'>
     <div className="group">
       <Group1 />
@@ -17,15 +26,17 @@ const Contact = () => (
         Feel free to get in touch with me to discuss your software development needs.
       </p>
     </div>
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         maxLength={30}
+        name="name"
         placeholder="Enter your full name"
         required
       />
       <input
         type="email"
+        name="email"
         placeholder="Enter your Enter your email address"
         required
       />
@@ -33,13 +44,14 @@ const Contact = () => (
         cols={30}
         rows={7}
         maxLength={500}
+        name="message"
         placeholder="Write your message here..."
         required
       />
-      <button type="submit" className="btn">Get In Touch</button>
+      <button type="submit" className="btn" disabled={state.submitting}>Get In Touch</button>
       <span id="errorMessage" />
     </form>
   </section>
-);
+)};
 
 export default Contact;
