a522cbfcc07dcb148ec8d7345a9e0392105ca114
LionRouge1 <matchoudiavlessi@gmail.com>
2024-03-11 23:09:40 +0000

    correct typo

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,7 +8,7 @@ const AboutMe = () => (
       <h1>About Myself</h1>
       <p>
         I'm a passionate and results-driven software developer with a deep love for creating efficient, user-friendly, and innovative solutions.
-        With 7 years of experience in the industry, I've honed my skills across a wide range of technologies and platforms.<br />
+        With more than 7 years of experience in the industry, I've honed my skills across a wide range of technologies and platforms.<br />
         I'm dedicated to continuous learning and staying at the forefront of industry trends. My enthusiasm for problem-solving, clean coding,
         and attention to detail has driven the success of the projects I've been involved in. Whether it's optimizing code for performance or creating elegant, user-centric designs, I'm always up for the task.
       </p>
