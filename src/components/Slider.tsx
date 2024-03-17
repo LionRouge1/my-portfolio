0273a2f647dfd19796c249cdff27f6ad51202392
LionRouge1 <matchoudiavlessi@gmail.com>
2024-03-17 11:52:06 +0000

    make popup's images responsive

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -6,7 +6,7 @@ const Slider = ({ images, name }: SliderProps) => {
   const [index, setIndex] = useState(0);
 
   const next = () => {
-    setIndex((prev) => prev >= (images.length -1) ? 0 : prev += 1);
+    setIndex((prev) => prev >= (images.length -1) ? images.length -1 : prev += 1);
   }
   const previous = () => {
     setIndex((prev) => prev <= 0 ? 0 : prev -= 1);
@@ -15,8 +15,8 @@ const Slider = ({ images, name }: SliderProps) => {
   return (
     <div className="popup-img">
       <img src={images[index]} alt={name} />
-      <button onClick={previous} type="button" className="btn next">&#10094;</button >
-      <button onClick={next} type="button" className="btn prev">&#10095;</button >
+      <button onClick={previous} type="button" className="btn next" style={{display: index <= 0 ? 'none':'inline-block'}}>&#10094;</button >
+      <button onClick={next} type="button" className="btn prev" style={{display: index >= (images.length -1) ? 'none':'inline-block'}}>&#10095;</button >
     </div>
   );
 };
